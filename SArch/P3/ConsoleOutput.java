4fd67925dffed32b895418084d67398b8220ce07
pocketinfinity <sthum@hm.edu>
2018-04-09 16:11:01 +0200

    singletons AGAIN

---
+++ SArch/P3/ConsoleOutput.java
@@ -0,0 +1,55 @@
+package edu.hm.cs.softarch.kwic.dip.output;
+
+import edu.hm.cs.softarch.kwic.dip.EntryListConsumer;
+import edu.hm.cs.softarch.kwic.dip.EntryListProvider;
+import edu.hm.cs.softarch.kwic.dip.KwicEntry;
+
+/**
+ * Klasse zur Ausgabe eines Wortes mit seinem Kontext.
+ * 
+ * @author katz.bastian
+ */
+public class ConsoleOutput implements EntryListConsumer {
+	
+	private static ConsoleOutput instance = null;
+	
+	private ConsoleOutput() {}
+	
+	public static ConsoleOutput getInstance() {
+		if (instance == null)
+			instance = new ConsoleOutput();
+		return instance;
+	}
+
+	/**
+	 * Gibt ein Wort mit umgebendem Kontext aus.
+	 * 
+	 * @param entryProvider
+	 *            Klasse, die sortierte Liste von Wörtern bereitstellt.
+	 */
+	@Override
+	public void take(EntryListProvider provider) {
+
+		for (KwicEntry kwicEntry : provider.getEntries()) {
+			StringBuffer sb = new StringBuffer();
+
+			// Das indizierte Wort
+			sb.append(kwicEntry.getWord());
+
+			// Rechts-Kontext
+			if (kwicEntry.hasPostContext()) {
+				sb.append(" ");
+				sb.append(String.join(" ", kwicEntry.getPostContext()));
+			}
+
+			// Linkt-Kontext, sofern vorhanden mit Komma trennen
+			if (kwicEntry.hasPreContext()) {
+				sb.append(", ");
+				sb.append(String.join(" ", kwicEntry.getPreContext()));
+			}
+
+			// Ausgabe
+			System.out.println(sb);
+		}
+	}
+}
---
+++ SArch/P3/Sorter.java
@@ -0,0 +1,43 @@
+package edu.hm.cs.softarch.kwic.dip;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Klasse zum Sortieren einer List von Kwic-Index-Einträgen.
+ * 
+ * @author katz.bastian
+ */
+public class Sorter implements EntryListFilter {
+
+	private List<KwicEntry> entries = new ArrayList<>();
+	private static Sorter instance = null;
+	
+	private Sorter() {}
+	
+	public static Sorter getInstance() {
+		if (instance == null)
+			instance = new Sorter();
+		return instance;
+	}
+
+	@Override
+	public void take(EntryListProvider provider) {
+		provider.getEntries().forEach(entries::add);
+		
+		entries.sort(new Comparator<KwicEntry>() {
+
+			@Override
+			public int compare(KwicEntry o1, KwicEntry o2) {
+				return o1.getWord().compareTo(o2.getWord());
+			}
+		});
+	}
+
+	@Override
+	public List<KwicEntry> getEntries() {
+		return Collections.unmodifiableList(entries);
+	}
+}
