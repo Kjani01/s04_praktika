58960a362978f915f8ef4032a77280d33929966b
pocketinfinity <sthum@hm.edu>
2018-04-11 10:18:45 +0200

    schönerererererer sourcekode

--- BS/Praktikum2/Aufgabe5.py
+++ BS/Praktikum2/Aufgabe5.py
@@ -8,15 +8,15 @@ def threadinc(_, amount):
 
 threads = []
 for i in range(0, 10):
-    amount = 1 - 2* (i % 2) #1 on even, -1 on odd i
+    amount = 1 - 2 * (i % 2) #1 on even, -1 on odd i
     t = threading.Thread(target=threadinc, args=(i, amount))
     t.name = "Thread No. " + str(i)
     threads.append(t)
     t.start()
 
 for t in threads:
-    print(t.name, " joined.")
     t.join()
+    print(t.name, " joined.")
 
 print ("===\nValue of global variable: ", a)
 
