6bfb5851a709fd9cf98afd5a57e4ffc76a727948
pocketinfinity <sthum@hm.edu>
2018-05-16 13:04:46 +0200

    library

--- BS/Praktikum4/library.py
+++ BS/Praktikum4/library.py
@@ -3,22 +3,22 @@
 
 import sys, threading
 
-def Job(i, stop_event):
+def Job(i):#, stop_event):
     print
-    print('Start CountJob nr:', i)
+    print("Student", i, "starts learning ...")
     print
-    while((not stop_event.is_set()):
-        pass
-    print('Job', i, 'exiting')
+    #while((not stop_event.is_set()):
+    #    pass
+    print("Student", i, "is done.")
 
 print("LIBRARY\n=======")
 numstudents = int(sys.argv[1])
 readingtime = int(sys.argv[2])
 print(numstudents, "students reading for", readingtime, "seconds each.")
 
-for i in range(0,400):
-    p = threading.Thread(target=Job, args=(i, stop_event))
+for i in range(0, numstudents):
+    p = threading.Thread(target=Job, args=(i))#, stop_event))
     #p.daemon = True
     p.start()
-time.sleep(10)
-stop_event.set()
+#time.sleep(10)
+#stop_event.set()
---
+++ BS/Praktikum4/library2.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import sys, threading, time
+
+def study(studentNo, delay):
+	global studentsstudying
+	count = 0
+	while (count < 5):
+		with book1:
+			with book2:
+				with book3:
+					with studentslock:
+						studentsstudying += 1
+					time.sleep(delay)
+					count += 1
+					with studylock:
+						studytimes[studentNo] = count
+					#print("Student", studentNo, "studied for", count, "seconds")
+					with studentslock:
+						studentsstudying -= 1
+
+print("LIBRARY\n=======")
+numstudents = int(sys.argv[1])
+readingtime = int(sys.argv[2])
+print(numstudents, "students reading for", readingtime, "seconds each.")
+
+
+book1 = threading.Semaphore(3)
+book2 = threading.Semaphore(2)
+book3 = threading.Semaphore(2)
+studentslock = threading.Semaphore(1)
+studentsstudying = 0
+studylock = threading.Semaphore(1)
+studytimes = []
+
+threads = []
+#try:
+for i in range(0, numstudents):
+	studytimes.append(0)
+	t = threading.Thread(target=study, args=(i, readingtime))
+	t.name = "Student #" + str(i)
+	threads.append(t)
+	t.start()
+#except:
+#	print ("Error: unable to start thread")
+
+while 1:
+	print("Students having all 3 books:", studentsstudying, " // Study time table:", studytimes)
+	time.sleep(1)
+	pass
