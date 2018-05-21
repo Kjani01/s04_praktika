3108ce68511ccf9c5bf55906d2dd9fad9a64332b
pocketinfinity <sthum@hm.edu>
2018-05-21 10:59:20 +0200

    ra6.2.b update

--- RA/Praktikum6/6.2.b.swift
+++ RA/Praktikum6/6.2.b.swift
@@ -1,5 +1,30 @@
 #!/usr/bin/swift
 
+extension String
+{
+    func leftPadding(toLength: Int, withPad character: Character) -> String
+    {
+        let stringLength = self.count
+        if stringLength < toLength
+        {
+            return String(repeatElement(character, count: toLength - stringLength)) + self
+        }
+        else
+        {
+            return String(self.suffix(toLength))
+        }
+    }
+}
+
+func printDouble(_ d: Double)
+{
+    print(d)
+    let bitPattern = d.bitPattern
+    let binary = String(bitPattern, radix: 2).leftPadding(toLength: 64, withPad: "0")
+    print(binary)
+    print()
+}
+
 var num = "0.1"
 
 while true
@@ -8,7 +33,12 @@ while true
     {
         if 1.0 + d != 1.0
         {
-            print(d)
+            printDouble(d)
+        }
+        let e = -d
+        if 1.0 + e != 1.0
+        {
+            printDouble(e)
         }
         num.insert("0", at: num.index(before: num.endIndex))
     }
