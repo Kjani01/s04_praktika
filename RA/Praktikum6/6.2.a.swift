591ccd524851c882ab125b4e0fd313cea2f89a8c
pocketinfinity <sthum@hm.edu>
2018-05-22 14:10:59 +0200

    ra62a super-ausführlich

--- RA/Praktikum6/6.2.a.swift
+++ RA/Praktikum6/6.2.a.swift
@@ -1,5 +1,7 @@
 #!/usr/bin/swift
 
+let verbose = false
+
 extension String
 {
     func leftPadding(toLength: Int, withPad character: Character) -> String
@@ -25,99 +27,43 @@ func printDouble(_ d: Double)
     print()
 }
 
-
-
-var num1 = "0.1"
-var num2 = "0.2"
-var num3 = "0.3"
-var num4 = "0.4"
-var num5 = "0.5"
-var num6 = "0.6"
-var num7 = "0.7"
-var num8 = "0.8"
-var num9 = "0.9"
-
-while true
+func smallestDouble(_ number: inout String) -> Double
 {
-    if let d = Double(num1)
+    var smallest = 1.0
+    while true
     {
-        printDouble(d)
-        num1.insert("0", at: num1.index(before: num1.endIndex))
+        if let d = Double(number)
+        {
+            if verbose
+            {
+                printDouble(d)
+            }
+            number.insert("0", at: number.index(before: number.endIndex))
+            if d < smallest
+            {
+                smallest = d
+            }
+        }
+        else
+        {
+            break
+        }
     }
-    else
+    return smallest
+}
+
+
+var numbers = ["0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9"]
+var smallest = 1.0
+
+for i in 0...numbers.count - 1
+{
+    let small = smallestDouble(&numbers[i])
+    if small < smallest
     {
-        break
-    }
-    if let d = Double(num2)
-    {
-        printDouble(d)
-        num2.insert("0", at: num2.index(before: num2.endIndex))
-    }
-    else
-    {
-        break
-    }
-    if let d = Double(num3)
-    {
-        printDouble(d)
-        num3.insert("0", at: num3.index(before: num3.endIndex))
-    }
-    else
-    {
-        break
-    }
-    if let d = Double(num4)
-    {
-        printDouble(d)
-        num4.insert("0", at: num4.index(before: num4.endIndex))
-    }
-    else
-    {
-        break
-    }
-    if let d = Double(num5)
-    {
-        printDouble(d)
-        num5.insert("0", at: num5.index(before: num5.endIndex))
-    }
-    else
-    {
-        break
-    }
-    if let d = Double(num6)
-    {
-        printDouble(d)
-        num6.insert("0", at: num6.index(before: num6.endIndex))
-    }
-    else
-    {
-        break
-    }
-    if let d = Double(num7)
-    {
-        printDouble(d)
-        num7.insert("0", at: num7.index(before: num7.endIndex))
-    }
-    else
-    {
-        break
-    }
-    if let d = Double(num8)
-    {
-        printDouble(d)
-        num8.insert("0", at: num8.index(before: num8.endIndex))
-    }
-    else
-    {
-        break
-    }
-    if let d = Double(num9)
-    {
-        printDouble(d)
-        num9.insert("0", at: num9.index(before: num9.endIndex))
-    }
-    else
-    {
-        break
+        smallest = small
     }
 }
+
+print("SMALLEST\n========")
+printDouble(smallest)
